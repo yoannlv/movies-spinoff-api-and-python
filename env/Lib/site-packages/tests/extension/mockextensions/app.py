350a538199d46d9e6d355a15a05a53bfc0f6bc20
Yoann Le Voguer <levoguer.yoann@gmail.com>
2021-01-07 11:48:13 +0100

    Ajout données

---
+++ env/Lib/site-packages/tests/extension/mockextensions/app.py
@@ -0,0 +1,44 @@
+import os
+from traitlets import Unicode, List
+
+from jupyter_server.base.handlers import JupyterHandler
+from jupyter_server.extension.application import (
+    ExtensionApp,
+    ExtensionAppJinjaMixin
+)
+from jupyter_server.extension.handler import (
+    ExtensionHandlerMixin,
+    ExtensionHandlerJinjaMixin
+)
+
+STATIC_PATH = os.path.join(os.path.dirname(__file__), "static")
+
+
+class MockExtensionHandler(ExtensionHandlerMixin, JupyterHandler):
+
+    def get(self):
+        self.finish(self.config.mock_trait)
+
+
+class MockExtensionTemplateHandler(
+    ExtensionHandlerJinjaMixin,
+    ExtensionHandlerMixin,
+    JupyterHandler
+):
+
+    def get(self):
+        self.write(self.render_template("index.html"))
+
+
+class MockExtensionApp(ExtensionAppJinjaMixin, ExtensionApp):
+
+    name = 'mockextension'
+    template_paths = List().tag(config=True)
+    static_paths = [STATIC_PATH]
+    mock_trait = Unicode('mock trait', config=True)
+    loaded = False
+
+    def initialize_handlers(self):
+        self.handlers.append(('/mock', MockExtensionHandler))
+        self.handlers.append(('/mock_template', MockExtensionTemplateHandler))
+        self.loaded = True
